9347a0659db5e567b059f4e8ccea185168b7be6d
ChehovaN <chhv.clb@gmail.com>
2021-03-01 09:56:29 +0300

    third commit

--- src/main/java/MyButtons.java
+++ src/main/java/MyButtons.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MyButtons extends JPanel {
 
@@ -7,5 +10,23 @@ public class MyButtons extends JPanel {
 
     public void SetObject(MyPanel butr) {
         this.butr = butr;
+    }
+
+    MyButtons() {
+        setLayout(new FlowLayout());
+        NewGame = new JButton("Новая игра");
+        Ex = new JButton("Выход");
+        NewGame.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                butr.Rese();
+            }
+        });
+        Ex.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                System.exit(0);
+            }
+        });
+        add(NewGame);
+        add(Ex);
     }
 }
--- src/main/java/MyPanel.java
+++ src/main/java/MyPanel.java
@@ -3,8 +3,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MyPanel extends JPanel implements ActionListener {
+
+    private JButton[] buttons;
+    private Play GArray;
+    private int PlayerMark = 1;
+    private int DrawCheck = 0;
+
     @Override
     public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void Rese() {
 
     }
 }
