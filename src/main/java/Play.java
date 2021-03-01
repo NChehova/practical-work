f54713615385c3719352f03974a1b311cde46e29
ChehovaN <chhv.clb@gmail.com>
2021-03-01 10:07:49 +0300

    fifth commit

--- src/main/java/Play.java
+++ src/main/java/Play.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Play {
 
     private MyPanel Pan;
@@ -15,6 +17,46 @@ public class Play {
     }
 
     public void InArray(int i, int j, int Check, int DrawCheck) {
+        PlayBoard[i][j] = Check;
+        ResultCheck(Check, DrawCheck);
+    }
 
+    public void ResultCheck(int Check, int DrawCheck) {
+        if ((PlayBoard[0][0] == Check && PlayBoard[0][1] == Check && PlayBoard[0][2] == Check) || (PlayBoard[1][0] == Check && PlayBoard[1][1] == Check && PlayBoard[1][2] == Check) || (PlayBoard[2][0] == Check && PlayBoard[2][1] == Check && PlayBoard[2][2] == Check)) {
+            if (Check == 1) {
+                JOptionPane.showMessageDialog(Pan, "1 ИГРОК ЧЕМПИОН");
+                k = 1;
+            } else if (Check == 2) {
+                JOptionPane.showMessageDialog(Pan, "2 ИГРОК ЧЕМПИОН");
+                k = 2;
+            }
+            Pan.AllButtons(false);
+
+        } else if ((PlayBoard[0][0] == Check && PlayBoard[1][0] == Check && PlayBoard[2][0] == Check) || (PlayBoard[0][1] == Check && PlayBoard[1][1] == Check && PlayBoard[2][1] == Check) || (PlayBoard[0][2] == Check && PlayBoard[1][2] == Check && PlayBoard[2][2] == Check)) {
+            if (Check == 1) {
+                JOptionPane.showMessageDialog(Pan, "1 ИГРОК ЧЕМПИОН");
+                k = 1;
+            } else if (Check == 2) {
+                JOptionPane.showMessageDialog(Pan, "2 ИГРОК ЧЕМПИОН");
+                k = 2;
+            }
+            Pan.AllButtons(false);
+
+        } else if ((PlayBoard[0][0] == Check && PlayBoard[1][1] == Check && PlayBoard[2][2] == Check) || (PlayBoard[2][0] == Check && PlayBoard[1][1] == Check && PlayBoard[0][2] == Check)) {
+            if (Check == 1) {
+                JOptionPane.showMessageDialog(Pan, "1 ИГРОК ЧЕМПИОН");
+                k = 1;
+
+            } else if (Check == 2) {
+                JOptionPane.showMessageDialog(Pan, "2 ИГРОК ЧЕМПИОН");
+                k = 2;
+
+            }
+            Pan.AllButtons(false);
+
+        } else if (DrawCheck == 8) {
+            JOptionPane.showMessageDialog(Pan, "НИЧЕЙКА");
+            k = 3;
+        }
     }
 }
