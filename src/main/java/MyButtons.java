be8aad86b3559a376fc5ca1818aa8bf578791d4a
ChehovaN <chhv.clb@gmail.com>
2021-03-01 09:51:58 +0300

    second commit

---
+++ src/main/java/MyButtons.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MyButtons extends JPanel {
+
+    private JButton NewGame, Ex;
+    private MyPanel butr;
+
+    public void SetObject(MyPanel butr) {
+        this.butr = butr;
+    }
+}
