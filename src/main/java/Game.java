6867e0e954c1345af7f7649e1fb96c25cd08b336
ChehovaN <chhv.clb@gmail.com>
2021-03-01 11:15:23 +0300

    six commit

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,18 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Game extends JFrame {
-    private MyPanel Pa;
-    private MyButtons Ex_RES;
 
     Game() {
         setLayout(new BorderLayout());
         setVisible(true);
         setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Pa = new MyPanel();
-        Ex_RES = new MyButtons();
-        Ex_RES.SetObject(Pa);
-        add(Ex_RES, BorderLayout.EAST);
-        add(Pa, BorderLayout.CENTER);
+        MyPanel pa = new MyPanel();
+        MyButtons ex_RES = new MyButtons();
+        ex_RES.SetObject(pa);
+        add(ex_RES, BorderLayout.EAST);
+        add(pa, BorderLayout.CENTER);
     }
 }
