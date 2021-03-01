d5b8d11d04b5c8a9fb6812435ee3db87a2f57f24
ChehovaN <chhv.clb@gmail.com>
2021-03-01 10:05:01 +0300

    fourth commit

--- src/main/java/MyPanel.java
+++ src/main/java/MyPanel.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,12 +10,74 @@ public class MyPanel extends JPanel implements ActionListener {
     private int PlayerMark = 1;
     private int DrawCheck = 0;
 
+    MyPanel() {
+        setLayout(new GridLayout(3, 3));
+        buttons = new JButton[9];
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i] = new JButton("");
+            buttons[i].setBackground(Color.YELLOW);
+        }
+        Rese();
+        for (int i=0; i < buttons.length; i++) {
+            add(buttons[i]);
+            buttons[i].addActionListener(this);
+        }
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
+        JButton Pressed = (JButton) e.getSource();
+        for (int button = 0; button <= 8; button++) {
+            if (Pressed == buttons[button]) {
+                MyText(Pressed, PlayerMark);
+                GArray.InArray(button / 3, button % 3, PlayerMark, DrawCheck);
+                PlayerMark = SwithcPlayer(PlayerMark);
+                ButtonFalse(buttons[button]);
+                DrawCheck = DrawCheck+1;
+            }
+        }
+    }
 
+    public void ButtonFalse(JButton Btn) {
+        Btn.setEnabled(false);
+    }
+
+    public int SwithcPlayer(int last) {
+        if (last == 2) {
+            PlayerMark = 1;
+            return 1;
+
+        } else {
+            PlayerMark = 2;
+            return 2;
+        }
+    }
+
+    public void AllButtons(boolean qq) {
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].setEnabled(qq);
+        }
+    }
+
+    public void EmptyText() {
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].setText("");
+        }
+    }
+
+    public void MyText(JButton Btn, int Play) {
+        if (Play == 2) {
+            Btn.setText("O");
+        } else if (Play == 1) {
+            Btn.setText("X");
+        }
     }
 
     public void Rese() {
-
+        GArray = new Play(this);
+        EmptyText();
+        AllButtons(true);
+        PlayerMark = 1;
+        DrawCheck = 0;
     }
 }
--- src/main/java/Play.java
+++ src/main/java/Play.java
@@ -1,2 +1,20 @@
 public class Play {
+
+    private MyPanel Pan;
+    private int PlayBoard[][];
+    public int k;
+
+    Play(MyPanel B) {
+        PlayBoard = new int[3][3];
+        Pan = B;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                PlayBoard[i][j] = 0;
+            }
+        }
+    }
+
+    public void InArray(int i, int j, int Check, int DrawCheck) {
+
+    }
 }
